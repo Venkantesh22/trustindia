88efdefc6cce4ff0d040583800ef4e0a3c4cfa8f
venkatesh22 <venkateshrathod242002@gmail.com>
2025-10-30 11:11:50 +0530

    subscription details UI done

--- lib/views/screens/product_details_screen/components/key_featured_section.dart
+++ lib/views/screens/product_details_screen/components/key_featured_section.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:lekra/controllers/product_controller.dart';
+import 'package:lekra/views/base/shimmer.dart';
 
 class KeyFeaturesSection extends StatelessWidget {
-  const KeyFeaturesSection({
-    super.key,
-  });
+  const KeyFeaturesSection({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetBuilder<ProductController>(builder: (productController) {
+      // final features = productController.isLoading
+      //     ? List.generate(4, (index) => "")
+      //     : (productController.productModel?.features ?? []);
+
       return Container(
         padding: const EdgeInsets.all(12),
         decoration: BoxDecoration(
@@ -23,25 +26,48 @@ class KeyFeaturesSection extends StatelessWidget {
               children: [
                 Icon(Icons.verified, color: Theme.of(context).primaryColor),
                 const SizedBox(width: 8),
-                Text("Key Features",
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleSmall
-                        ?.copyWith(fontWeight: FontWeight.bold)),
+                Text(
+                  "Key Features",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleSmall
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                ),
               ],
             ),
             const SizedBox(height: 12),
-            Wrap(
-                runSpacing: 8,
-                spacing: 8,
-                children: List.generate(
-                  productController.isLoading
-                      ? 4
-                      : (productController.productModel?.features?.length ?? 0),
-                  (index) => FeatureChip(
-                      text: productController.productModel?.features?[index] ??
-                          ""),
-                )),
+            ListView.separated(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: productController.isLoading
+                  ? 2
+                  : (productController.productModel?.features?.length ?? 0),
+              separatorBuilder: (_, __) => const SizedBox(height: 8),
+              itemBuilder: (context, index) {
+                final featureText = productController.isLoading
+                    ? ""
+                    : productController.productModel?.features?[index];
+                return CustomShimmer(
+                  isLoading: productController.isLoading,
+                  child: Row(
+                    children: [
+                      const Icon(Icons.circle, size: 8, color: Colors.black),
+                      const SizedBox(width: 8),
+                      Expanded(
+                        child: Text(
+                          featureText ?? "",
+                          textAlign: TextAlign.start,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(fontSize: 15),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           ],
         ),
       );
@@ -49,16 +75,16 @@ class KeyFeaturesSection extends StatelessWidget {
   }
 }
 
-class FeatureChip extends StatelessWidget {
-  final String text;
-  const FeatureChip({required this.text});
+// class FeatureChip extends StatelessWidget {
+//   final String text;
+//   const FeatureChip({required this.text});
 
-  @override
-  Widget build(BuildContext context) {
-    return Chip(
-      label: Text(text, style: Theme.of(context).textTheme.bodyMedium),
-      backgroundColor: Theme.of(context).cardColor,
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Chip(
+//       label: Text(text, style: Theme.of(context).textTheme.bodyMedium),
+//       backgroundColor: Theme.of(context).cardColor,
+//       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+//     );
+//   }
+// }
--- lib/views/screens/subscription_plan/subscription_details_screen/subscription_details_screen.dart
+++ lib/views/screens/subscription_plan/subscription_details_screen/subscription_details_screen.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import 'package:lekra/controllers/subscription_controller.dart';
 import 'package:lekra/services/constants.dart';
 import 'package:lekra/services/theme.dart';
+import 'package:lekra/views/base/shimmer.dart';
+import 'package:lekra/views/screens/dashboard/profile_screen/profile_screen.dart';
 import 'package:lekra/views/screens/widget/custom_appbar/custom_appbar2.dart';
 
 class SubscriptionDetailsScreen extends StatefulWidget {
@@ -52,6 +54,104 @@ class _SubscriptionDetailsScreenState extends State<SubscriptionDetailsScreen> {
                         fontWeight: FontWeight.w700,
                         fontSize: 36,
                       ),
+                ),
+                const SizedBox(
+                  height: 28,
+                ),
+                Text(
+                  "A smart start for saving on your favorite products",
+                  style: Helper(context).textTheme.bodyMedium?.copyWith(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 12,
+                      ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(PriceConverter.convertToNumberFormat(3999),
+                    style: Helper(context).textTheme.bodyMedium?.copyWith(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                          color: grey,
+                          decoration: TextDecoration.lineThrough,
+                          decorationColor: grey,
+                          decorationThickness: 2,
+                        )),
+                Text(
+                  "${subscriptionController.selectSubscription?.priceFormat ?? ""}/month",
+                  style: Helper(context).textTheme.bodyMedium?.copyWith(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ProfileButton(
+                    title: "Activate Plan",
+                    onPressed: () {},
+                    color: primaryColor),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 30),
+                  child: Divider(
+                    color: grey,
+                  ),
+                ),
+                Text(
+                  "Benefits You Get",
+                  style: Helper(context).textTheme.bodyMedium?.copyWith(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                      ),
+                ),
+                ListView.separated(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    final featuresText = subscriptionController.isLoading
+                        ? ""
+                        : subscriptionController
+                            .selectSubscription?.features?[index];
+                    return CustomShimmer(
+                      isLoading: subscriptionController.isLoading,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Icon(
+                              Icons.circle,
+                              size: 10,
+                              color: black,
+                            ),
+                            const SizedBox(width: 8),
+                            Expanded(
+                              child: Text(
+                                featuresText ?? "",
+                                textAlign: TextAlign
+                                    .start, 
+                                style: Helper(context)
+                                    .textTheme
+                                    .bodyMedium
+                                    ?.copyWith(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (_, __) => const SizedBox(
+                    height: 15,
+                  ),
+                  itemCount: subscriptionController.isLoading
+                      ? 2
+                      : (subscriptionController
+                              .selectSubscription?.features?.length ??
+                          0),
                 )
               ],
             ),
