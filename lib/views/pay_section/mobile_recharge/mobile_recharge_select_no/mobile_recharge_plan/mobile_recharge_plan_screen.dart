1d506e7dacf0ddf4080e8974722d3487bb0f4f4d
venkatesh22 <venkateshrathod242002@gmail.com>
2026-03-27 18:45:21 +0530

    replace app bar

--- lib/views/pay_section/mobile_recharge/mobile_recharge_select_no/mobile_recharge_plan/mobile_recharge_plan_screen.dart
+++ lib/views/pay_section/mobile_recharge/mobile_recharge_select_no/mobile_recharge_plan/mobile_recharge_plan_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lekra/services/constants.dart';
-import 'package:lekra/services/theme.dart';
 import 'package:lekra/views/pay_section/mobile_recharge/mobile_recharge_select_no/mobile_recharge_plan/widget/recharge_plan_section.dart';
 import 'package:lekra/views/pay_section/mobile_recharge/mobile_recharge_select_no/mobile_recharge_plan/widget/recharge_plan_top_section.dart';
+import 'package:lekra/views/screens/widget/custom_appbar/custom_appbar2.dart';
 
 class MobileRechargePlanScreen extends StatefulWidget {
   const MobileRechargePlanScreen({super.key});
@@ -16,22 +16,7 @@ class _MobileRechargePlanScreenState extends State<MobileRechargePlanScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: appbarBlueLight,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new),
-          onPressed: () => pop(context),
-          color: blueLight,
-        ),
-        title: Text(
-          "Mobile Recharge plan",
-          style: Helper(context).textTheme.headlineSmall?.copyWith(
-                fontSize: 18,
-                fontWeight: FontWeight.w700,
-                color: blueLight,
-              ),
-        ),
-      ),
+      appBar: CustomAppBar2(title: "Mobile Recharge plan"),
       body: Padding(
         padding: AppConstants.screenPadding,
         child: Column(
